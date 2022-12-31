d2c59f20031698e5f9e9f7166f288745edef240d
Ronaldo <roengsilva@gmail.com>
2022-12-31 10:55:43 -0300

    Criação de TRecord

---
+++ app.ado/TRecord.class.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * Classe TRecord
+ * Esta classe provê os métodos necessários para persistir e recuperar 
+ * objetos da base de dados (Active Record) 
+ */
+abstract class TRecord
+{
+   /**
+    * método __construct()
+    * instancia um Active Record. Se passado o $id, ja carrega o objeto
+    *  @param [$id] = ID do objeto
+    */ 
+    public function __construct($id = NULL)
+    {
+        if ($id) // se o ID for informado
+        {
+            // carrega o objeto correspondente
+            $object = $this->load($id);
+
+            if($object)
+            {
+                $this->fromArray($object->toArray());
+            }
+        }
+    }
+
+    /**
+     * método __clone()
+     * executado quando o objeto for clonado
+     * limpa o ID para que seja gerado um novo ID para o clone
+     */
+    public function __clone()
+    {
+        unset($this->id);
+    }
+
+    /**
+     * método __set()
+     * executado sempre que uma propriedade for atribuida
+     */
+    private function __set($prop, $value)
+    {
+        // Verifia se existe método set_<proprety>
+        if(method_exists($this, 'set_'.$prop))
+        {
+            //executa o método set_<proprety>
+            call_user_func(array($this, 'set_'.$prop, $value));
+        }
+        else
+        {
+            // atribui o valor da propriedade
+            $this->data[$prop] = $value;
+        }
+    }
+
+    /**
+     * MÉTODO __get($prop)
+     * executado sempre que uma propriedade for requerida
+     */
+    private function __get($prop)
+    {
+        // verifica se existe método get_<property>
+        if(method_exists($this, 'get_'.$prop))
+        {
+            // executa o método get_<property>
+            return call_user_func(array($this, 'get_'.$prop));
+        }
+        else
+        {
+            // retorna o valor da propriedade
+            return $this->data[$prop];
+        }
+    }
+
+    /**
+     * método getEntity()
+     * retorna o nome da entidade (tabela)
+     */
+    private function getEntity()
+    {
+        // obtém o nome da classe
+        $class = strtolower(get_class($this));
+        // retorna o nome da classe - "Record"
+        return substr($class, 0, -6);
+    }
+
+    /**
+     * método fromArray()
+     * preeche os dados do objeto com um array
+     */
+    public function formArray($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * método toArray
+     * retorna os dados do objeto como array
+     */
+    public function toArray()
+    {
+        return $this->data;
+    }
+
+    /**
+     * método store()
+     * armazena o objeto na base de dados e retorna o número de
+     * linha afetadas pela instrução SQL (zero ou um)
+     */
+    public function store()
+    {
+        // verifica se ten ID ou se existe na base de dados
+        if(empty($this->data['id']) or (!$this->load($this->id)))
+        {
+            // incrementa o ID
+            $this->id = $this->getLast() +1;
+
+            // cria uma instrução de insert
+            $sql = new TSqlInsert;
+            $sql->setEntity($this->getEntity());
+
+
+            // percorre os dados do objeto sql
+            foreach($this->data as $key => $value)
+            {
+                // passa os dados do objeto para o SQL
+                $sql->setRowData($key, $this->$key);
+            }
+        }
+        else
+        {
+            // instancia instrução de update
+            $sql = new TSqlUpdate;
+            $sql->setEntity($this->getEntity());
+            
+            // cria um critério de seleção baseado no ID
+            $criteria = new TCriteria;
+            $criteria->add(new TFilter('id', '=', $this->id));
+            $sql->setCriteria($criteria);
+
+            // percorre os dados do objeto
+            foreach($this->data as $key => $value)
+            {
+                if($key !== 'id')  // o ID não precisa ir no UPDATE
+                {
+                    // passa os dados do objeto para o SQL
+                    $sql->setRowData($key, $this->$key);
+                }
+            }
+        }
+
+        // obtem a transação ativa
+        if($conn = TTransaction::get())
+        {
+            // faz o log e executa o SQL
+            TTransaction::log($sql->getInstruction());
+            $result = $conn->exec($sql->getInstruction());
+
+            // retorna o resultado
+            return $result;
+        }
+        else
+        {
+            // se não tiver transação, retorna uma exceção
+            throw new Exception('Não há transação ativa!!');
+        }
+    }
+
+    /**
+     * método load()
+     * recupera (retorna) um objeto da base de dados
+     * através de seu ID e instancia ele na memória
+     *  @param $id = ID do objeto 
+     */
+    public function load($id)
+    {
+        // instancia instrução SELECT
+        $sql = new TSqlSelect;
+        $sql->setEntity($this->getEntity());
+        $sql->addColumn('*');
+
+        // cria critério para seleção baseado no ID
+        $criteria = new TCriteria;
+        $criteria->add(new TFilter('id', '=', $id));
+
+        // difine o critério deseleção de dados
+        $sql->setCriteria($criteria);
+
+        // obtem transação ativa
+        if($conn = TTransaction::get())
+        {
+            // cria mensagem de log e executa a consulta
+            TTransaction::log($sql->getInstruction());
+            $result = $conn->Query($sql->getInstruction());
+
+            // se retornou algum dado
+            if($result)
+            {
+                // retorna os dados em forma de objeto
+                $object = $result->fetchObject(get_class($this));
+            }
+
+            return $object;
+        }
+        else
+        {
+            // se não tiver transação, retorna uma exceção 
+            throw new Exception('Não há transação ativa!!');
+        }
+    }
+
+
+}
--- data_mapper.php
+++ data_mapper.php
@@ -24,5 +24,102 @@ final class Product
         $this->stock = $stock;
         $this->cost_price = $cost_price;
     }
-    
+
+    /**
+     * Método getDescription()
+     * retorna a descrição do produto
+     */
+    public  function getdescription()
+    {
+        return $this->description;
+    }
 }
+
+/**
+ * Classe Sale
+ * representa uma venda de produtos
+ */
+final class Sale
+{
+    private $id;
+    private $items;
+
+    /**
+     * @param $id
+     */
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Método getId()
+     * retorna o identificador
+     */
+    function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Método addItem()
+     * adiciona um item na cesta
+     *  @param $quantity = quantidade vendida
+     *  @param $product = objeto produto
+     */
+    public function addItem($quantity, Product $product)
+    {
+        $this->items[] = array($quantity, $product);
+    }
+
+    /**
+     * Método getItems()
+     * retorna o array de itens da cesta
+     */
+    public  function getItems()
+    {
+        return $this->items;
+    }
+}
+
+/**
+ * Classe SaleMapper
+ *implementa Data Mapper para a classe Venda
+ */
+final class SaleMapper
+{
+    function insert(Sale $sale)
+    {
+        $id = $sale->getId();
+        $date = date("d/m/Y");
+
+        // Insere a venda no banco de dados
+        $sql = "INSERT INTO sale (id, date) VALUES ('$id', '$date')";
+        echo $sql . "<br>\n";
+
+        // percorre os itens vendidos
+        foreach ($sale->getItems() as $item)
+        {
+            $quantity = $item[0];
+            $product = $item[1];
+            $description = $product->getDescription();
+
+            //insere os itens da venda no banco de dados
+            $sql = "INSERT INTO sale_items(ref_sale, product, quantity)" .
+                "VALUES ('$id', '$description', '$quantity')";
+
+            echo $sql . "<br>\n";
+        }
+    }
+}
+
+// instancia objeto Venda
+$sale = new Sale(1000);
+
+// adiciona os produtos
+$sale->addItem(3, new Product('Vinho', 10, 15));
+$sale->addItem(2, new Product('Salame', 20, 20));
+$sale->addItem(1, new Product('Queijo', 30, 10));
+
+// Data Mapper persiste a venda
+SaleMapper::insert($sale);
